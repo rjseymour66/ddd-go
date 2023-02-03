ca7931463619281ce55456d23bba1c9f82f6ae57
rseymour <rjs.github@gmail.com>
2023-02-02 23:43:42 -0500

    implementing the repo

--- coffeco/internal/purchase/repository.go
+++ coffeco/internal/purchase/repository.go
@@ -2,8 +2,27 @@ package purchase
 
 import (
 	"context"
+	"fmt"
 )
 
+// Repository provides flexibility about which data store we use.
 type Repository interface {
 	Store(ctx context.Context, purchase Purchase) error
+}
+
+type MongoRepository struct {
+	purchases *mongo.Collection
+}
+
+func NewMongoRepo(ctx context.Context, connectionString string) (*MongoRepository, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a mongo client: %w", err)
+	}
+
+	purchases := client.Database("coffeeco").Collection("purchases")
+
+	return &MongoRepository{
+		purchases: purchases,
+	}, nil
 }
